11ffc7ffd776a73c84945b6c1b1ccdf9ccb55157
MazaBast <MazaBast@users.noreply.github.com>
2017-12-21 11:46:03 +0100

    4e Version stable

--- app/src/main/java/com/example/mazan/androboom/UserActivityList.java
+++ app/src/main/java/com/example/mazan/androboom/UserActivityList.java
@@ -6,14 +6,20 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,17 +38,64 @@ public class UserActivityList extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            TextView tv = new TextView(getContext());
-            tv.setText(liste.get(position).getEmail());
-            return tv;
+
+ /*           TextView tv = new TextView(getContext());             tv.setText(liste.get(position).getEmail());             return tv; */
+
+            // on va chercher le bon profil dans la liste
+
+            Profil p = liste.get(position);
+
+            // on instancie le layout sous la forme d'un objet de type View
+            View layout = View.inflate(getContext(), R.layout.profil_list_item, null);
+            // on va chercher les trois composants du layout
+            ImageView imageProfilView = (ImageView) layout.findViewById(R.id.imageView);
+            TextView textView = (TextView) layout.findViewById(R.id.textView);
+            ImageView imageConnectedView = (ImageView) layout.findViewById(R.id.imageView2);
+
+            // on télécharge dans le premier composant l'image du profil
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference photoRef = storage.getReference().child(p.getEmail() + "/photo.jpg");
+            if (photoRef != null) {
+                Glide.with(getContext()).using(new FirebaseImageLoader())
+                        .load(photoRef)
+                        .skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .placeholder(R.drawable.ic_person_black_24dp)
+                        .into(imageProfilView);
+            }
+            // on positionne le email dans le TextView
+            textView.setText(p.getEmail());
+            // si l'utilisateur n'est pas connecté, on rend invisible le troisième
+            // composant
+            if (!p.isConnected) {
+                imageConnectedView.setVisibility(View.INVISIBLE);
+            }
+
+            // on retourne le layout
+            return layout;
         }
+            // on va chercher le bon profil dans la liste
+
+
+/*
+            // on instancie le layout sous la forme d'un objet de type View
+            View layout = View.inflate(getContext(), R.layout.profil_list_item, null);
+            // on va chercher les trois composants du layout
+            ImageView imageProfilView = (ImageView) layout.findViewById(R.id.imageView);
+            TextView textView = (TextView) layout.findViewById(R.id.textView);
+            ImageView imageConnectedView = (ImageView) layout.findViewById(R.id.imageView2);
+            // on retourne le layout
+            return layout;
+        }
+
+*/
+
+
 
         @Override
         public int getCount() {
             return liste.size();
-        }
+        }}
 
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +117,8 @@ public class UserActivityList extends AppCompatActivity {
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {                 // Getting Post failed, log a message
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
                 Log.v("AndroBoum", "loadPost:onCancelled", databaseError.toException());
             }
         };
